2ebf0bd5c7e727fbc108a69a13981e793fa212c8
TDSFugal <tim.smith@fugaltech.com>
2019-12-02 20:41:23 -0500

    Created initial parking lot class

---
+++ src/service/__init__.py
@@ -0,0 +1,3 @@
+"""The service namespace."""
+
+from .parking_lot import ParkingLot  # noqa: F401
---
+++ src/service/parking_lot.py
@@ -0,0 +1,8 @@
+"""internal service for parking lots."""
+
+
+class ParkingLot:
+    """Parking lot class."""
+
+    def __init__(self):
+        """Construct a parking lot."""
