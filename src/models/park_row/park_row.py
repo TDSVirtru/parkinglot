a1dbb5b0647fa7e26f47ebe6dd1c459ac2b1f09c
TDSFugal <tim.smith@fugaltech.com>
2019-12-04 00:23:54 -0500

    Working commit on park row. Need to upgrade park a bit first.

--- src/models/park_row/park_row.py
+++ src/models/park_row/park_row.py
@@ -1,8 +1,44 @@
 """internal service for park rows."""
 
+from ..park import Park
 
-class ParkRow:
+
+class ParkRow(Park):
     """ParkRow class."""
 
-    def __init__(self):
+    def __init__(self, name, spaces=[]):
         """Construct a park row."""
+        Park.__init__(self, name)
+        self.__spaces = set(spaces)
+
+    @property
+    def spaces(self):
+        """Return a copy of the set of spaces."""
+        return self.__spaces.copy()
+
+    @spaces.setter
+    def spaces(self, value):
+        """Noop."""
+        raise Exception("Cannot set spaces on a park row after construction.")
+
+    def park(self, car):
+        """Park the car if possible.
+
+        Returns the coordinates of the space if successful, or None if
+        the space is full or the car doesn't have the attributes required.
+        """
+        return None
+
+    def is_full(self):
+        """Return the full status of the row instance."""
+        for space in self.__spaces:
+            if space.is_empty():
+                return False
+        return True
+
+    def is_empty(self):
+        """Return the empty status of the row instance."""
+        for space in self.__spaces:
+            if space.is_full():
+                return False
+        return True
--- src/models/park_row/park_row_test.py
+++ src/models/park_row/park_row_test.py
@@ -4,7 +4,74 @@ import pytest  # noqa: F401
 
 from .park_row import ParkRow
 
+from ..park_space import ParkSpace
+from ..car import Car
+
 
 # @pytest.mark.skip()
-def test_park_row_construction():
-    assert type(ParkRow()) is ParkRow
+def test_park_row_construction_empty():
+    foo = ParkRow("Fred")
+    assert type(foo) is ParkRow
+    assert foo.name == "Fred"
+    assert len(foo.spaces) == 0
+
+
+# @pytest.mark.skip()
+def test_park_row_construct_with_spaces():
+    space1 = ParkSpace("One")
+    space2 = ParkSpace("Two")
+    space3 = ParkSpace("Three")
+    foo = ParkRow("Fred", [space1, space2, space3])
+    assert type(foo) is ParkRow
+    assert foo.name == "Fred"
+    assert len(foo.spaces) == 3
+    assert space1 in foo.spaces
+    assert space2 in foo.spaces
+    assert space3 in foo.spaces
+
+
+# @pytest.mark.skip()
+def test_park_row_no_spaces_status():
+    """Check the degenerate case.
+
+    If no spaces then the row is both full and empty.
+    """
+    foo = ParkRow("Fred")
+    assert foo.is_empty()
+    assert foo.is_full()
+
+
+# @pytest.mark.skip()
+def test_park_row_status_with_spaces():
+    space = ParkSpace("One")
+    foo = ParkRow("Fred", [space])
+    assert foo.is_empty()
+    assert not foo.is_full()
+    car = Car()
+    foo.park(car)
+    assert not foo.is_empty()
+    assert foo.is_full()
+    
+
+@pytest.mark.skip()
+def test_park_row_park_qualified_car():
+    space1 = ParkSpace("One")
+    space2 = ParkSpace("Two")
+    space3 = ParkSpace("Three")
+    foo = ParkRow("Fred", [space1, space2, space3])
+    car1 = Car()
+    car2 = Car()
+    car3 = Car()
+    assert foo.is_empty()
+    assert foo.park(car1) == "Fred-One"
+    assert not foo.is_full()
+    assert not foo.is_empty()
+
+
+@pytest.mark.skip()
+def test_park_row_reject_park_if_full():
+    foo = ParkRow("Fred", [])  # A regular Row with no attributes
+    car1 = Car()  # a regular car with no attributes
+    car2 = Car()  # a regular car with no attributes
+    foo.park(car1)
+    assert foo.park(car2) is None
