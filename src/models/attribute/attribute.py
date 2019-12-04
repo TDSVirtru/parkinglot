42bde84848cfdd508d2ca725505baadf359eb201
TDSFugal <tim.smith@fugaltech.com>
2019-12-03 19:53:19 -0500

    Implemented abstract Attribute base class

--- src/models/attribute/attribute.py
+++ src/models/attribute/attribute.py
@@ -1,8 +1,36 @@
 """Attribute model."""
 
+PREFERS = "prefers"
+ACCEPTS = "accepts"
+REJECTS = "rejects"
+
 
 class Attribute:
-    """Attribute class."""
+    """The abstract Attribute class."""
 
-    def __init__(self):
-        """Construct a Attribute."""
+    def __init__(self, name=None):
+        """Construct a Attribute with a value string."""
+        self.__name = name
+
+    @property
+    def attribute(self):
+        """Return the attribute name."""
+        return self.__name
+
+    @attribute.setter
+    def attribute(self, value):
+        """NOOP the request to protect the internal name value."""
+        pass
+
+    def permits(self, Car):
+        """Determine if a Space attribute permits the car to park.
+
+        Acceptable responses are the boolean values True and False.
+        """
+        raise Exception("Abstract method Attribute.permits() not defined")
+
+    def desires(self, Space):
+        """Determine how much a Car attribute desires a space.
+
+        Acceptable responses are PREFERS, ACCEPTS, and REJECTS."""
+        raise Exception("Abstract method Attribute.desires() not defined")
--- src/models/attribute/attribute_test.py
+++ src/models/attribute/attribute_test.py
@@ -6,5 +6,31 @@ from .attribute import Attribute
 
 
 # @pytest.mark.skip()
-def test_Attribute_construction():
-    assert type(Attribute()) is Attribute
+def test_attribute_construction_empty():
+    foo = Attribute()
+    assert type(foo) is Attribute
+    assert foo.attribute is None
+
+
+def test_attribute_construction_with_name():
+    foo = Attribute("FOO")
+    assert type(foo) is Attribute
+    assert foo.attribute == "FOO"
+
+
+def test_attribute_name_durability():
+    foo = Attribute("FOO")
+    foo.attribute = "BAR"
+    assert foo.attribute == "FOO"
+
+
+def test_attribute_abstraction_for_permits():
+    foo = Attribute()
+    with pytest.raises(Exception):
+        foo.permits(None)
+
+
+def test_attribute_abstraction_for_desires():
+    foo = Attribute()
+    with pytest.raises(Exception):
+        foo.desires(None)
